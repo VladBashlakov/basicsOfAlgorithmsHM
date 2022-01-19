11555837ae58adaee6b9360c29d9efcf0028abee
Vlad <VladBashlakov@gmail.com>
2022-01-19 19:04:40 +0300

    Initial commit

---
+++ main/java/com/example/basicsofalgorithmshm/BasicsOfAlgorithmsHmApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsofalgorithmshm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsOfAlgorithmsHmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsOfAlgorithmsHmApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/basicsofalgorithmshm/BasicsOfAlgorithmsHmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsofalgorithmshm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsOfAlgorithmsHmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
